0c5c62f405d012f410828c1cb98dcf7c08a7bb10
kabhishek2023@gmail.com <kabhishek2023@gmail.com>
2022-12-09 13:51:39 +0530

    Updated the middileware

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,7 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require("cors");
+
 const connectToDB = require("./config/db");
 const userRoutes = require("./routes/userRoutes");
 const app = express();
@@ -7,8 +9,11 @@ const app = express();
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
+// Connection To Db
 connectToDB();
+
 app.use("/", userRoutes);
 
 module.exports = app;
